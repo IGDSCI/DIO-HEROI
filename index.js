fb68a312a3340b2e59bb5151a32f39a1152eaa3f
Gustavo dos Santos Capeletti <gustavocapeletti007@gmail.com>
2024-10-10 14:28:57 -0300

    Desafio Heroi

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('teste')
+var nome = "Gustavo"
+var experiencia = 10001
+var nivel
+
+if (experiencia < 1000){
+    nivel = "Ferro"
+} else if (1001 <= experiencia && experiencia <= 2000){
+    nivel = "Bronze"
+} else if (2001 <= experiencia && experiencia <= 5000){
+    nivel = "Prata"
+} else if (5001 <= experiencia && experiencia <= 7000){
+    nivel = "Ouro"
+} else if (7001 <= experiencia && experiencia <= 8000){
+    nivel = "Platina"
+} else if (8001 <= experiencia && experiencia <= 9000){
+    nivel = "Ascendente"
+} else if (9001 <= experiencia && experiencia <= 10000){
+    nivel = "Imortal"
+} else{
+    nivel = "Radiante"
+}
+
+console.log('O Herói de nome ' + nome + ' está no nível ' + nivel)
